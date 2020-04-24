7eb42ad1837c8f5d999e56f3de0689f342dace8a
nawedzaman <zamannawed28@gmail.com>
2020-04-24 13:22:33 +0530

    NPM scripts part 2

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 <script>
     $(document).ready(function(){
-        $("#mycarousel").carousel( { interval: 2000 } );
+        $("#mycarousel").carousel( { interval: 2000 } )
         $("#carouselButton").click(function(){
             if ($("#carouselButton").children("span").hasClass('fa-pause')) {
                 $("#mycarousel").carousel('pause');
